b4c0b0391712196bd4c7d9afcb506badf6e8c805
e2e2a <emonawong22@gmail.com>
2024-03-14 20:39:33 +0800

    add

--- controllers/admin/enrollmentController.js
+++ controllers/admin/enrollmentController.js
@@ -23,45 +23,54 @@ module.exports.index = async (req, res) => {
 }
 
 module.exports.create = async (req, res) => {
+    const userLogin = await User.findById(req.session.login);
     res.render('admin/enrollmentCreate', {
         req: req,
         messages: req.flash(),
+        userLogin: userLogin,
     });
 }
 
 module.exports.doCreate = async (req, res) => {
-    const course = await Course.findById(req.body.courseId);
-    const existingEnrollment = await Enrollement.findOne({
-        userId: req.session.login,
-        courseId: req.body.courseId
-    });
-    if (!existingEnrollment || existingEnrollment.status === 'done' || existingEnrollment.status === 'cancelled') {
-        const currentDate = new Date();
-        const formattedDate = currentDate.toISOString().split('T')[0];
-        const enroll = new Enrollement({
-            userId: req.session.login,
-            courseId: req.body.courseId,
-            courseTitle: course.title,
+    const titleChecking = await Course.find({ title: req.body.courseTitle })
+    console.log(req.body.courseTitle)
+    console.log(titleChecking)
+    if (titleChecking && titleChecking.length > 0) {
+        const existingEnrollment = await Enrollement.findOne({
             name: req.body.name,
-            address: req.body.address,
-            placeDeath: req.body.placeDeath,
-            age: req.body.age,
-            contact: req.body.contact,
-            fatherName: req.body.fatherName,
-            motherName: req.body.motherName,
-            level: req.body.level,
-            schedule: req.body.schedule,
-            time: req.body.time,
-            dateEnrolling: formattedDate,
-            isApproved: 'false',
+            courseTitle: req.body.courseTitle
         });
-        await enroll.save();
-        console.log('success enrollment');
-        req.flash('message', 'Enrollement Successful!');
-        return res.redirect('/courses');
+        if (!existingEnrollment || existingEnrollment.status === 'done' || existingEnrollment.status === 'cancelled') {
+            const currentDate = new Date();
+            const formattedDate = currentDate.toISOString().split('T')[0];
+            const enroll = new Enrollement({
+                userId: req.session.login,
+                courseTitle: req.body.courseTitle,
+                name: req.body.name,
+                address: req.body.address,
+                placeDeath: req.body.placeDeath,
+                age: req.body.age,
+                contact: req.body.contact,
+                fatherName: req.body.fatherName,
+                motherName: req.body.motherName,
+                level: req.body.level,
+                schedule: req.body.schedule,
+                time: req.body.time,
+                dateEnrolling: formattedDate,
+                isApproved: 'false',
+            });
+            await enroll.save();
+            console.log('success enrollment');
+            req.flash('message', 'Enrollement Successful!');
+            return res.redirect('/admin/enrollment');
+        } else {
+            console.log('user Already apply this enrollment');
+            req.flash('message', 'You are already enrolled in this course.');
+            return res.redirect('/admin/enrollment/create');
+        }
     } else {
-        console.log('user Already apply this enrollment');
-        req.flash('message', 'You are already enrolled in this course.');
-        return res.redirect('/courses');
+        console.log('failed no title exist in course');
+        req.flash('message', 'Title does not Exist!');
+        return res.redirect('/admin/enrollment/create');
     }
 }
